ac8ea46f7b0c630ff153e13d3ab39c3e4556daaf
DESKTOP-8CKDL9U\HABUR <Hector>
2021-10-26 19:46:21 -0500

    crear servicio get.

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 
-const port = 4000;
+const port = 7000;
 
 const server = new express();
+server.use(bodyParser.json);
+
+let respuesta ={
+    error: false,
+    codigo: 200,
+    mensaje: ''
+};
+
+server.get('/', function(request, response){
+    respuesta ={
+        error: false,
+        codigo: 200,
+        mensaje: ' Servicio git raiz no utilizar'
+    };
+    response.send(respuesta);
+});
 
 server.listen(port, () => {
     console.log("Servidor iniciado -> " + port);
