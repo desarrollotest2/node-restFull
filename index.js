1132f585b76266e00a45f8d7f531136866c698d0
DESKTOP-8CKDL9U\HABUR <Hector>
2021-10-26 20:46:04 -0500

    formateo

--- index.js
+++ index.js
@@ -6,23 +6,23 @@ const port = 4000;
 
 server.use(bodyParser.json());
 
-let respuesta ={
-    error:false,
-    codigo:200,
-    mensaje:''
+let respuesta = {
+    error: false,
+    codigo: 200,
+    mensaje: ""
 };
 
 
-server.get('/', function(request, response){
-    respuesta ={
-        error:true,
-        codigo:200,
-        mensaje:'Servicio git raiz no utilizar'
+server.get('/', function (request, response) {
+    respuesta = {
+        error: true,
+        codigo: 200,
+        mensaje: 'Servicio git raiz no utilizar'
     }
     response.send(respuesta);
 
 });
 
-server.listen(port, ()=>{
-    console.log("Servidor iniciado ->"+port);
+server.listen(port, () => {
+    console.log("Servidor iniciado ->" + port);
 });
